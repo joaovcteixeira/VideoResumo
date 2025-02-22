6e52898e63f640da9de62d551e86d897adcfdce5
João Teixeira <joao.correia2014gbi@hotmail.com>
2025-02-22 02:23:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def teste_youtube():
         return jsonify({'erro': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=10000)
+    app.run(host='0.0.0.0', port=5000)
